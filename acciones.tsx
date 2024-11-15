4a5d4431a4dea32b7b5d229c319b1b5d7be47875
Omar <omar.sk80@gmail.com>
2024-11-15 15:45:28 -0300

    16.ProtegerAccionesServidorClerk

--- acciones.tsx
+++ acciones.tsx
@@ -3,12 +3,17 @@
 import { db } from "@/db";
 import { Facturas } from "@/db/schema";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 export async function crearAccion(formData: FormData) {
-    
+    const { userId } = await auth();
     // console.log('datos Formulario:', formData);
     const valor = Math.floor(parseFloat(String(formData.get('valor')))*1000);
     const descripcion = formData.get('descripcion') as string;
+
+    if ( !userId) {
+        return;
+    }
 
     const resultados = await db.insert(Facturas)
         .values({
